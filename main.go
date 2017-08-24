85cfc127b039a69c2ab91d91aa2a5d5b98cbbdc8
David Sosa <davidjsosav@gmail.com>
2017-08-23 21:53:48 -0400

    Changed grid from slice to array. Added comments to functions. Added the GetBox function. Added the FindPosibleSolutions function.

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	grid := make([][]int, 9)
+	var grid [9][9]int
 	for i := 0; i < 9; i++ {
-		grid[i] = make([]int, 9)
+		grid[i] = [9]int{}
 		for j := 0; j < 9; j++ {
 			if len(gridStr[i][j]) > 0 {
 				num, err := strconv.ParseInt(gridStr[i][j], 10, 64)
@@ -34,9 +34,18 @@ func main() {
 		}
 	}
 	PrintGrid(grid)
+	for x := 0; x < 9; x++ {
+		for y := 0; y < 9; y++ {
+			posibleSolutions := FindPosibleSolutions(x, y, grid)
+			if len(posibleSolutions) == 1 {
+				fmt.Printf("Definitive solution for (%d, %d): %v\n", x, y, posibleSolutions)
+			}
+		}
+	}
 }
 
-func PrintGrid(grid [][]int) {
+// PrintGrid prints the complete Sudoku grid.
+func PrintGrid(grid [9][9]int) {
 	for i, hLine := range grid {
 		if i%3 == 0 {
 			fmt.Println("  -------------------------- ")
@@ -52,4 +61,71 @@ func PrintGrid(grid [][]int) {
 		}
 	}
 	fmt.Println("  -------------------------- ")
+}
+
+// GetBox takes a position and a grid and returns the box that position belongs to.
+// Each position having their respective value.
+func GetBox(x, y int, grid [9][9]int) [3][3]int {
+	var box [3][3]int
+	x -= x % 3
+	y -= y % 3
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			box[i][j] = grid[x+i][y+j]
+		}
+	}
+	return box
+}
+
+// FindPosibleSolutions calculates the posible solutions for a single position in the grid.
+//
+// If the position already has a value, then this function returns an empty slice.
+func FindPosibleSolutions(x, y int, grid [9][9]int) []int {
+	// if the position is already filled, there's no need to find any other solution
+	if grid[x][y] != 0 {
+		return []int{}
+	}
+	// start with all posible solutions
+	posibleSolutions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// find numbers in the same line (either vertical or horizontal)
+	for i := 0; i < 9; i++ {
+
+		// horizontal line check
+		if grid[x][i] != 0 {
+			// remove from posibleSolutions slice
+			for index, element := range posibleSolutions {
+				if element == grid[x][i] {
+					posibleSolutions = append(posibleSolutions[:index], posibleSolutions[index+1:]...)
+					break
+				}
+			}
+		}
+
+		// vertical line check
+		if grid[i][y] != 0 {
+			// remove from posibleSolutions slice
+			for index, element := range posibleSolutions {
+				if element == grid[i][y] {
+					posibleSolutions = append(posibleSolutions[:index], posibleSolutions[index+1:]...)
+					break
+				}
+			}
+		}
+	}
+	// find numbers in the same box
+	boxX := x - (x % 3)
+	boxY := y - (y % 3)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			elem := grid[boxX+i][boxY+j]
+			for i := range posibleSolutions {
+				if posibleSolutions[i] == elem {
+					posibleSolutions = append(posibleSolutions[:i], posibleSolutions[i+1:]...)
+					break
+				}
+			}
+		}
+	}
+	return posibleSolutions
 }
